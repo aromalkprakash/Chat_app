7ccdbbeea5e25e40e8878eef79afa2a955a5fea4
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-06-03 21:26:06 +0530

    mssgmodel

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -52,8 +52,8 @@ export const signup = async (req, res) => {
 };
 
 
-export const login = async (req , res) => {
-    try {
+export const login = async (req, res) => {
+	try {
 		const { username, password } = req.body;
 
 		const user = await User.findOne({ username });
@@ -65,26 +65,26 @@ export const login = async (req , res) => {
 
 		generateTokenAndSetCookie(user._id, res);
 		
-			res.status(201).json({
-				_id: user._id,
-				fullName: user.fullName,
-				username: user.username,
-				profilePic: user.profilePic,
-			});
+		res.status(201).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePic: user.profilePic,
+		});
 
 
 	} catch (error) {
 		console.log("Error in signup controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
-}
+};
 
 export const logout = (req, res) => {
-    try {
+	try {
 		res.cookie("jwt", "", ({ maxAge: 0 }));
 		res.status(200).json({ message: "Logged out successfully" });
 	} catch (error) {
 		console.log("Error in logout controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
-}
+};
