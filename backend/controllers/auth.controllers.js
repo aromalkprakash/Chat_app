364aa8726b2405a42ccc95b1808bd0d2b2fbc2c6
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-06-03 19:43:37 +0530

    login

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs"
-import generateTokenAndSetCookie from "../utils/generateToken.js"
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
 	try {
@@ -16,8 +16,8 @@ export const signup = async (req, res) => {
 			return res.status(400).json({ error: "Username already exists" });
 		}
 
-		const salt = await bcryptjs.genSalt(10);
-		const hashedPassword = await bcryptjs.hash(password, salt);
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
 
 		
 		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -52,8 +52,32 @@ export const signup = async (req, res) => {
 };
 
 
-export const login = (res, req) => {
-    res.send("login")
+export const login = async (req , res) => {
+    try {
+		const { username, password } = req.body;
+
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(password, user.password || "");
+
+		if (!user || !isPasswordCorrect) {
+			return res.status(400).json({ error: "Invalid credentials" });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+			
+
+			res.status(201).json({
+				_id: user._id,
+				fullName: user.fullName,
+				username: user.username,
+				profilePic: user.profilePic,
+			});
+
+
+	} catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 }
 
 export const logout = (res, req) => {
