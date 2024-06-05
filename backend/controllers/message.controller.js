50969d2d948948f8c08a5b2426faaa27efca77ed
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-06-05 14:28:37 +0530

    get mssg

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -39,3 +39,26 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({ error: "internal server issue"});
     }
 }
+
+
+export const getMessage = async (req, res) => {
+    try {
+        const { id: userToChat } = req.params;
+        const senderId = req.user._id;
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChat] }
+        }).populate("messages");
+
+
+        if (!conversation)  return res.status(200).json([]);
+        
+        const messages = conversation.messages
+
+        res.status(200).json(messages);
+
+
+    } catch (error) {
+        console.log("Error in get message controller", error.message);
+        res.status(500).json({ error: "internal server issue"});
+    }
+}
