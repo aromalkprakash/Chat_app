1c93a127b27661d24d5252635b04a2ab0448d0fc
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-06-06 11:50:15 +0530

    login f starter

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -8,6 +8,31 @@ const Login = () => {
                     Login
                     <span className='text-green-500'> Aura</span>
                 </h1>
+                <form>
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>Username</span>
+                        </label>
+                        <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+                    </div>
+                    
+                    <div>
+                    <label className='label'>
+							<span className='text-base label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'/>
+                    </div>
+                    <a  href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+						{"Don't"} have an account?
+					</a>
+                    <div>
+                        <button className='btn btn-block btn-sm mt-2' > Login
+                            </button>
+                    </div>
+                </form>
             </div>
         </div>
     )
